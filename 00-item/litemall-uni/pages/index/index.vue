d62ac417f0a49f4163655ed7a3b0325c1a8071dd
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-16 13:03:39 +0800

    新增：创建litemall-uni项目，并使用uni-ui组件库，完成首页-搜索框渲染

---
+++ 00-item/litemall-uni/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="container">
+		<uni-search-bar readonly placeholder="点击前往搜索" bgColor="#f7f8fa" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
