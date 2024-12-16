6646ab213bb1170f03fa3337cc52e12ec920c405
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-16 18:25:00 +0800

    新增：完成搜索框渲染

--- 00-item/litemall-uni/pages/category/category.vue
+++ 00-item/litemall-uni/pages/category/category.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		
+	<view class="category">
+		<!-- 搜索栏 -->
+		<view class="search">
+			<uni-search-bar readonly placeholder="点击前往搜索" bgColor="#f7f8fa" />
+		</view>
+		<!-- 页面主要内容 -->
 	</view>
 </template>
 
@@ -17,6 +21,15 @@
 	}
 </script>
 
-<style>
-
+<style scoped lang="scss">
+	// 页面颜色
+	.category {
+		background-color: #fff;
+		width: 100vw;
+		height: calc(100vh - 100rpx);
+	}
+// 搜索栏
+.search {
+	background-color: #fff;
+}
 </style>
