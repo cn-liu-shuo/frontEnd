c6365b9be040cd81c7af3df2b2d32952deb0d2d4
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-16 09:47:44 +0800

    新增：重新创建 litemall-uni 项目

--- 00-item/litemall-uni/main.js
+++ 00-item/litemall-uni/main.js
@@ -3,10 +3,6 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
-import uView from './uni_modules/uview-ui'
-Vue.use(uView)
-// 如此配置即可
-uni.$u.config.unit = 'rpx'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
