23f3029f032f296da903b9294a4c0181af677a1b
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-17 19:03:59 +0800

    新增：完成常见问题页面

---
+++ 00-item/litemall-uni/pages/help/help.vue
@@ -0,0 +1,57 @@
+<template>
+	<view>
+		<!-- 顶部导航 -->
+		<van-nav-bar left-text="返回" left-arrow @click-left="onClickLeft" />
+		<!-- 问题列表 -->
+		<van-collapse v-model="activeName" accordion>
+			<van-collapse-item v-for="item in list" :key="item.id" :title="item.question">{{item.answer}}</van-collapse-item>
+		</van-collapse>
+	</view>
+</template>
+
+<script>
+	import {
+		NavBar,
+		Collapse,
+		CollapseItem
+	} from 'vant';
+	import {
+		IssueList
+	} from '../../config/api';
+	export default {
+		components: {
+			[NavBar.name]: NavBar,
+			[Collapse.name]: Collapse,
+			[CollapseItem.name]: CollapseItem
+		},
+		data() {
+			return {
+				activeName: '1',
+				list: []
+			}
+		},
+		onLoad() {
+			this.init()
+		},
+		methods: {
+			// 初始化
+			init() {
+				IssueList().then(res => {
+					if (res.errmsg === '成功') {
+						console.log(res);
+						this.list = res.data.list
+					}
+				})
+			},
+			// 返回
+			onClickLeft() {
+				// 返回上一页
+				uni.navigateBack()
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
